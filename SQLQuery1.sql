d1338cce6c03a15a8fbe5ed1d8988821a6bfc407
devof313 <134063162+devof313@users.noreply.github.com>
2023-06-18 21:45:51 +0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TableRegester]
+(
+	[Id] INT NOT NULL, 
+    [email] VARCHAR(50) NULL, 
+    [name] VARCHAR(50) NULL, 
+    [roll] VARCHAR(50) NULL, 
+    [birthday] DATE NULL, 
+    [username] VARCHAR(50) NULL, 
+    [passcode] VARCHAR(50) NULL
+)
---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Tablelogin]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [passcode] VARCHAR(50) NULL
+)
